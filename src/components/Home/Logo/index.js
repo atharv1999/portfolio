8ee6dac2a37902032e99f2da4606a15cdfaabe66
D1_62880_Atharv <atharvarote@gmail.com>
2022-04-03 18:14:06 +0530

    deploy4

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,43 +1,15 @@
 import './index.scss'
 import LogoA from '../../../assets/images/logo-a.png'
-import { useRef } from 'react';
 
 
 const Logo = () => {
 
-    const bgRef = useRef();
-    const outlineLogoRef = useRef();
-    const solidLogoRef = useRef();
 
-    // useEffect(() => {
-    //     gsap.registerPlugin(DrawSVGPlugin)
-
-    //     gsap.timeline()
-    //     .to(bgRef.current, {
-    //         duration: 1,
-    //         opacity: 1,
-    //     })
-    //     .from(outlineLogoRef.current, {
-    //         drawSVG:0,
-    //         duration:20,
-    //     })
-
-    //     gsap.fromTo(
-    //         solidLogoRef.current, {
-    //             opacity:0,
-    //         },{
-    //             opacity:1,
-    //             delay:4,
-    //             duration:4,
-    //         }
-    //     )
-
-    // },[])
 
 
     return (
-        <div className='logo-container' ref={bgRef}>
-            <img ref={solidLogoRef} className='solid-logo' src={LogoA} alt='A'></img>
+        <div className='logo-container'>
+            <img className='solid-logo' src={LogoA} alt='A'></img>
             {/* <svg
         width="559pt"
         height="897pt"
